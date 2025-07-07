0e64a4774c17f2c22e4bf37370099a073c9fdb86
mjjm4d <threehalves1@gmail.com>
2025-07-07 15:45:00 -0500

    Player script rename and small refactor

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,96 +7,111 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    private Vector2 moveDelta = new Vector2();
-    private CharacterController cc;
-    private bool isJumping = false;
-    private bool isRolling = false;
-    private bool rollHeld = false;
-    [SerializeField] private float moveSpd;
-    [SerializeField] private float maxSpd;
-    [SerializeField] private float runSpd;
-    [SerializeField] private float friction;
-    [SerializeField] private float rollSpd;
-    [SerializeField] private float airRollSpd;
-    [SerializeField] private float rollTime;
-    [SerializeField] private float gravity;
-    [SerializeField] private float rollingGravity;
-    [SerializeField] private float jumpHeight;
-    [SerializeField] private float rollingJumpHeight;
-    private bool groundedLastFrame = false;
-    private Vector2 dashMoveDelta;
-    private float curDashTime = 0f;
-    [SerializeField] private float dashCooldown;
-    private bool intangible = false;
-    private float intangibleTime = 0f;
+    // Component references
+    [Header("Component References")]
     [SerializeField] private HealthDisp hpDisp;
+    [SerializeField] private Transform cameraTransform;
+    [SerializeField] TMPro.TMP_Text scoreText;
+    private CharacterController _characterController;
 
-    private bool hitThisFrame = false;
-    [SerializeField] SpriteRenderer[] playerSprites;
+    // controller input vector
+    private Vector3 _rawMoveDelta = new();
+    // Transformed input vector based on camera position 
+    private Vector3 _moveDelta = new();
+    // Input vector stored at start of a roll
+    private Vector3 _rollMoveDelta = new();
+
+    // Jump is input but player has not performed jumped yet
+    private bool _willJump = false;
+    private bool _willRoll = false;
+    private bool _rollHeld = false;
+
+    // Player controls constants
+    [Header("Control Constants")]
+    [SerializeField] private float _walkSpeed;
+    [SerializeField] private float _maxRunSpeed;
+    [SerializeField] private float _runAccel;
+    [SerializeField] private float _runFriction;
+    [SerializeField] private float _gravity;
+    [SerializeField] private float _jumpHeight;
+
+    [SerializeField] private float _rollSpeed;
+    [SerializeField] private float _airRollSpeed;
+    [SerializeField] private float _rollDuration;
+    [SerializeField] private float _rollCooldown;
+    [SerializeField] private float _rollingGravity;
+    [SerializeField] private float _rollingJumpHeight;
+    
+    private bool _groundedLastFrame = false;
+    private bool _hitThisFrame = false;
+    private bool _isIntangible = false;
+
+    private float _currentRollTimer = 0f;
+    private float _intangibleTimer = 0f;
+
+    // Visual references
+    [Header("Visual References")]
+    [SerializeField] private SpriteRenderer[] playerSprites;
     [SerializeField] private Sprite bootWalkSprite;
     [SerializeField] private Sprite bootJumpSprite;
-    private Vector3 startingScale;
 
     [SerializeField] private Animator animator;
     [SerializeField] private Transform playerVisual;
     [SerializeField] private ParticleSystem deathParicles;
 
     [SerializeField] private Shake shake;
+    private Vector3 startingScale;
+    private SpriteRenderer playerHeadSR;
 
-    public int score = 0;
-    public float timeSurvived = 0;
+    [NonSerialized] public int score = 0;
+    [NonSerialized] public float timeSurvived = 0;
 
+    // Sound effects
+    [Header("Sound Effects")]
     [SerializeField] GameObject dashSFX;
     [SerializeField] GameObject stepSFX;
     [SerializeField] GameObject hurtSFX;
     [SerializeField] GameObject deathSFX;
 
-    [SerializeField] TMPro.TMP_Text scoreText;
-
-    private SpriteRenderer playerHeadSR;
-
     // Start is called before the first frame update
     void Start()
     {
-        cc = GetComponent<CharacterController>();
+        _characterController = GetComponent<CharacterController>();
         startingScale = playerVisual.localScale;
         playerHeadSR = playerVisual.transform.GetChild(2).GetComponent<SpriteRenderer>();
     }
 
     void FixedUpdate()
     {
-        // Lateral input direction
-        Vector3 v = new Vector3(moveDelta.x, 0, moveDelta.y).normalized;
         // Amount we will move player at the end of tick
-        Vector3 delta = Vector3.zero;
+        Vector3 delta;
 
         // running movement
-        if (rollHeld && !isRolling && curDashTime <= 0)
+        if (_rollHeld && !_willRoll && _currentRollTimer <= 0)
         {
             // move player with acceleration
-            delta = GetMovementVector(v, cc.velocity);
+            delta = GetMovementVector(_moveDelta, _characterController.velocity);
         }
         // If not running, do basic walk/roll controls
         else
         {
-            delta = v * moveSpd;
-            if (curDashTime > 0)
+            delta = _moveDelta * _walkSpeed;
+            if (_currentRollTimer > 0)
             {
-                delta = new Vector3(dashMoveDelta.x, 0, dashMoveDelta.y).normalized * (cc.isGrounded ? rollSpd : airRollSpd);
+                delta = _rollMoveDelta * (_characterController.isGrounded ? _rollSpeed : _airRollSpeed);
             }
         }
 
         // jumping and gravity
-        delta.y = cc.velocity.y * Time.fixedDeltaTime;
-        if (isJumping && cc.isGrounded)
+        delta.y = _characterController.velocity.y * Time.fixedDeltaTime;
+        if (_willJump && _characterController.isGrounded)
         {
-            delta.y = curDashTime <= 0 ? jumpHeight : rollingJumpHeight;
-            isJumping = false;
+            delta.y = _currentRollTimer <= 0 ? _jumpHeight : _rollingJumpHeight;
+            _willJump = false;
         }
+        delta.y -= _currentRollTimer <= 0 ? _gravity : _rollingGravity;
 
-        delta.y -= curDashTime <= 0 ? gravity : rollingGravity;
-
-        cc.Move(delta);
+        _characterController.Move(delta);
     }
 
     // Update is called once per frame
@@ -106,54 +121,57 @@ public class Player : MonoBehaviour
         timeSurvived += Time.deltaTime;
 
         // start a roll
-        if (isRolling && curDashTime <= -dashCooldown && moveDelta != Vector2.zero)
+        if (_willRoll && _currentRollTimer <= -_rollCooldown && _moveDelta != Vector3.zero)
         {
-            curDashTime = rollTime;
-            intangibleTime = rollTime;
-            dashMoveDelta = moveDelta.normalized;
-            isRolling = false;
+            _currentRollTimer = _rollDuration;
+            _intangibleTimer = _rollDuration;
+            _rollMoveDelta = _moveDelta;
+            _willRoll = false;
             Instantiate(dashSFX);
         }
 
-        intangible = intangibleTime > 0;
+        _isIntangible = _intangibleTimer > 0;
         // continue dash in air
-        if (cc.isGrounded) 
+        if (_characterController.isGrounded) 
         {
-            curDashTime -= Time.deltaTime;
-            intangibleTime -= Time.deltaTime;
+            _currentRollTimer -= Time.deltaTime;
+            _intangibleTimer -= Time.deltaTime;
         }
         // stop air dash on land
-        if (cc.isGrounded && !groundedLastFrame)
+        if (_characterController.isGrounded && !_groundedLastFrame)
         {
-            curDashTime = 0f;
-            intangibleTime = 0f;
+            _currentRollTimer = 0f;
+            _intangibleTimer = 0f;
         }
 
-        // temp for debug
-        if (intangible) playerHeadSR.color = Color.green;
+        UpdateVisuals();
+
+        _hitThisFrame = false;
+        _groundedLastFrame = _characterController.isGrounded;
+    }
+
+    // update player visuals and animation variables
+    private void UpdateVisuals()
+    {
+        // Change head color to indicate iframes
+        if (_isIntangible) playerHeadSR.color = Color.green;
         else playerHeadSR.color = Color.white;
 
-        // update player visuals and animations
-        if (moveDelta.x != 0) playerVisual.transform.localScale = new Vector3((moveDelta.x > 0) ? startingScale.x : -startingScale.x, startingScale.y, startingScale.z);
-        animator.SetBool("Walk", (moveDelta != Vector2.zero) && curDashTime <= 0 && cc.isGrounded && !rollHeld);
-        animator.SetBool("Roll", curDashTime > 0);
-        animator.SetBool("Run", rollHeld && !isRolling && curDashTime <= 0 && (moveDelta != Vector2.zero));
-        animator.SetBool("InAir", !cc.isGrounded);
-        animator.SetFloat("YVel", cc.velocity.y);
+        if (_moveDelta.x != 0) playerVisual.transform.localScale = new Vector3((_moveDelta.x > 0) ? startingScale.x : -startingScale.x, startingScale.y, startingScale.z);
+        animator.SetBool("Walk", (_moveDelta != Vector3.zero) && _currentRollTimer <= 0 && _characterController.isGrounded && !_rollHeld);
+        animator.SetBool("Roll", _currentRollTimer > 0);
+        animator.SetBool("Run", _rollHeld && !_willRoll && _currentRollTimer <= 0 && (_moveDelta != Vector3.zero));
+        animator.SetBool("InAir", !_characterController.isGrounded);
+        animator.SetFloat("YVel", _characterController.velocity.y);
 
         // shoe sprite
-        Sprite s = cc.isGrounded ? bootWalkSprite : bootJumpSprite;
-        // shoe layer order
-        int o = (cc.isGrounded || curDashTime > 0) ? 1 : 3;
+        Sprite s = _characterController.isGrounded ? bootWalkSprite : bootJumpSprite;
+        // shoe layer order, either behind or in front of head
+        int o = (_characterController.isGrounded || _currentRollTimer > 0) ? 1 : 3;
         playerSprites[0].sprite = s;
         playerSprites[1].sprite = s;
         playerSprites[0].sortingOrder = o;
         playerSprites[1].sortingOrder = o;
-
-        hitThisFrame = false;
-        groundedLastFrame = cc.isGrounded;
-        scoreText.text = score.ToString();
-
     }
 
     private Vector3 AddAcceleration(Vector3 inputDir, Vector3 currentVel, float acceleration, float maxVel)
@@ -177,7 +195,7 @@ public class Player : MonoBehaviour
             Vector3 lateralVel = Vector3.Scale(currentVel, new Vector3(1, 0, 1));
             if (lateralVel.magnitude != 0)
             {
-                float d = lateralVel.magnitude * friction * Time.fixedDeltaTime;
+                float d = lateralVel.magnitude * _runFriction * Time.fixedDeltaTime;
                 currentVel.x *= Mathf.Max(lateralVel.magnitude - d, 0) / lateralVel.magnitude;
                 currentVel.z *= Mathf.Max(lateralVel.magnitude - d, 0) / lateralVel.magnitude;
             }
@@ -185,15 +203,15 @@ public class Player : MonoBehaviour
             return AddAcceleration(
                 inputDir,
                 currentVel,
-                runSpd / Time.fixedDeltaTime,
-                maxSpd / Time.fixedDeltaTime
+                _runAccel / Time.fixedDeltaTime,
+                _maxRunSpeed / Time.fixedDeltaTime
                 );
         }
 
     public bool Hurt(bool ignoresIntangible = false)
     {
-        if ((intangible && !ignoresIntangible) || hitThisFrame) return false;
-        hitThisFrame = true;
+        if ((_isIntangible && !ignoresIntangible) || _hitThisFrame) return false;
+        _hitThisFrame = true;
         hpDisp.hp -= 1;
         hpDisp.hpObjects[hpDisp.hp].SetActive(false);
         hpDisp.timeSinceHit = Time.time;
@@ -203,7 +221,7 @@ public class Player : MonoBehaviour
             return true;
         }
 
-        intangibleTime = 1f;
+        _intangibleTimer = 1f;
         shake.StartShake(0.3f, 0.3f);
         score = (int)Mathf.Ceil(score * 0.75f);
         Instantiate(hurtSFX);
@@ -215,26 +233,34 @@ public class Player : MonoBehaviour
     {
         playerHeadSR.color = Color.clear;
         scoreText.text = "";
-        moveDelta = Vector2.zero;
+        _moveDelta = Vector2.zero;
         animator.SetBool("Walk", false);
         deathParicles.Play();
         Instantiate(deathSFX);
         StartCoroutine(GameObject.Find("PostGameParent").GetComponent<PostGameWindow>().StartPostGame());
     }
 
-    void OnMove(InputValue v)
+    public void CalculateMoveInputDir()
     {
-        moveDelta = v.Get<Vector2>();
+        if (cameraTransform == null) return;
+        _moveDelta = Quaternion.AngleAxis(cameraTransform.rotation.eulerAngles.y, Vector3.up) * _rawMoveDelta;
+    }
+
+    void OnMove(InputValue value)
+    {
+        var v = value.Get<Vector2>();
+        _rawMoveDelta = new Vector3(v.x, 0, v.y).normalized;
+        CalculateMoveInputDir();
     }
 
     void OnJump(InputValue v)
     {
-        isJumping = v.isPressed;
+        _willJump = v.isPressed;
     }
 
     void OnRoll(InputValue v)
     {
-        isRolling = v.isPressed;
-        rollHeld = v.isPressed;
+        _willRoll = v.isPressed;
+        _rollHeld = v.isPressed;
     }
 }
